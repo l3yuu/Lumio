acb6a00f5f29b417c18d26d0d89744b747a48c1d
l3yuu <binas.leumar1@gmail.com>
2026-07-03 23:29:48 +0800

    chore: update compiled Python files in __pycache__ directories

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Table, Text, JSON, DateTime, LargeBinary
 from sqlalchemy.orm import relationship
 from datetime import datetime
